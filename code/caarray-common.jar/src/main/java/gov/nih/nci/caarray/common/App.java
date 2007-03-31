811145e59d192bb4e6253474f68eab5e3cd08257
Scott Miller <smiller@5amsolutions.com>
2007-03-31 00:25:56 +0000

    added cobertura code coverage to the build

--- code/caarray-common.jar/src/main/java/gov/nih/nci/caarray/common/App.java
+++ code/caarray-common.jar/src/main/java/gov/nih/nci/caarray/common/App.java
@@ -4,10 +4,9 @@ package gov.nih.nci.caarray.common;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public String getMessage() {
+        return "Hello World!!";
     }
 }
--- code/caarray-services.jar/src/main/java/gov/nih/nci/caarray/services/App.java
+++ code/caarray-services.jar/src/main/java/gov/nih/nci/caarray/services/App.java
@@ -4,10 +4,5 @@ package gov.nih.nci.caarray.services;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
 }
