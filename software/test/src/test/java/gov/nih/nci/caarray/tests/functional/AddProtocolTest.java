d7196ac1104d8adb672ac56555bf3ad635ae1160
Rashmi Srinivasa <rashmi.srinivasa@nih.gov>
2007-07-10 19:31:51 +0000

    Changed DDL, Hibernate mapping, POJO and test code to match changed model for Protocol.

--- software/test/src/test/java/gov/nih/nci/caarray/tests/functional/AddProtocolTest.java
+++ software/test/src/test/java/gov/nih/nci/caarray/tests/functional/AddProtocolTest.java
@@ -16,14 +16,16 @@ public class AddProtocolTest extends AbstractSeleniumTest {
         selenium.click("protocolMenu:addProtocol");
         selenium.waitForPageToLoad("30000");
         selenium.type("protocolForm:protocolName", "new protocol");
-        selenium.type("protocolForm:protocolTitle", "new protocol title");
-        selenium.type("protocolForm:protocolText", "new protocol text");
+        selenium.type("protocolForm:protocolDescription", "new protocol description");
+        selenium.type("protocolForm:protocolContact", "new protocol contact");
+        selenium.type("protocolForm:protocolHardware", "new protocol hardware");
+        selenium.type("protocolForm:protocolSoftware", "new protocol software");
         selenium.type("protocolForm:protocolUrl", "http://www.5amsolutions.com");
         selenium.click("protocolForm:saveButton");
         selenium.waitForPageToLoad("30000");
         verifyTrue(selenium.isTextPresent("new protocol"));
         assertEquals("View Protocol", selenium.getTitle());
-        verifyTrue(selenium.isTextPresent("new protocol text"));
+        verifyTrue(selenium.isTextPresent("new protocol description"));
     }
 
     @Test
@@ -32,13 +34,13 @@ public class AddProtocolTest extends AbstractSeleniumTest {
         selenium.click("protocolForm:modifyButton");
         selenium.waitForPageToLoad("30000");
         selenium.type("protocolForm:protocolName", "updated protocol");
-        selenium.type("protocolForm:protocolText", "updated protocol text");
+        selenium.type("protocolForm:protocolDescription", "updated protocol description");
         selenium.type("protocolForm:protocolUrl", "http://updated.5amsolutions.com");
         selenium.select("protocolForm:protocolType", "label=fractionate");
         selenium.click("protocolForm:saveButton");
         selenium.waitForPageToLoad("30000");
         verifyTrue(selenium.isTextPresent("updated protocol"));
-        verifyTrue(selenium.isTextPresent("updated protocol text"));
+        verifyTrue(selenium.isTextPresent("updated protocol description"));
         verifyTrue(selenium.isTextPresent("http://updated.5amsolutions.com"));
         verifyTrue(selenium.isTextPresent("fractionate"));
         selenium.click("protocolMenu:searchProtocol");
