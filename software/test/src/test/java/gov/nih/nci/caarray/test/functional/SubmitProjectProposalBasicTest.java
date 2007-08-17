f30f02426eb84ee85ceab57f8d9b7a478c43a6ba
Eric Tavela <eric.tavela2@nih.gov>
2007-08-17 13:21:05 +0000

    Moved test

--- software/test/src/test/java/gov/nih/nci/caarray/test/functional/SubmitProjectProposalBasicTest.java
+++ software/test/src/test/java/gov/nih/nci/caarray/test/functional/SubmitProjectProposalBasicTest.java
@@ -80,7 +80,7 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package gov.nih.nci.caarray.test.functional.usecase.annotation.projectmanagement;
+package gov.nih.nci.caarray.test.functional;
 
 import gov.nih.nci.caarray.test.base.AbstractSeleniumTest;
 
