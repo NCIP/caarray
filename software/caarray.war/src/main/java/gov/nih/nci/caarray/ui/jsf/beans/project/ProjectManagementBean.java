15fc410f72d5fb339de653451ffed619fb89bd23
Eric Tavela <eric.tavela2@nih.gov>
2007-08-30 12:08:38 +0000

    Revised project API to retrieve projects belonging to user -- DAO implementation must be updated when CSM is in place to support this. Updated tests to account for login.

--- software/caarray.war/src/main/java/gov/nih/nci/caarray/ui/jsf/beans/project/ProjectManagementBean.java
+++ software/caarray.war/src/main/java/gov/nih/nci/caarray/ui/jsf/beans/project/ProjectManagementBean.java
@@ -142,7 +142,7 @@ public final class ProjectManagementBean implements Serializable {
      * @return all projects
      */
     public List<Project> getProjects() {
-        return getProjectManagementService().getAll();
+        return getProjectManagementService().getWorkspaceProjects();
     }
 
     /**
