73783b0460f6112c6c73ff347cb6ceef5fa068db
John Hedden <heddenj@mail.nih.gov>
2007-09-28 17:59:27 +0000

    removed unused references

--- software/caarray.war/src/main/java/gov/nih/nci/caarray/web/delegate/ManageFilesDelegate.java
+++ software/caarray.war/src/main/java/gov/nih/nci/caarray/web/delegate/ManageFilesDelegate.java
@@ -1,7 +1,6 @@
 package gov.nih.nci.caarray.web.delegate;
 
 import gov.nih.nci.caarray.application.file.FileManagementService;
-import gov.nih.nci.caarray.application.project.ProjectManagementService;
 import gov.nih.nci.caarray.util.j2ee.ServiceLocator;
 
 /**
@@ -12,20 +11,7 @@ import gov.nih.nci.caarray.util.j2ee.ServiceLocator;
 public class ManageFilesDelegate extends BaseDelegate {
 
     private ServiceLocator locator = ServiceLocator.INSTANCE;
-    private ProjectManagementService projectManagementService;
     private FileManagementService fileManagementService;
-
-    /**
-     * Get ProjectManagementService.
-     * @return projectManagementService
-     */
-    public ProjectManagementService getProjectManagementService() {
-        if (projectManagementService == null) {
-            projectManagementService =
-                (ProjectManagementService) locator.lookup(ProjectManagementService.JNDI_NAME);
-        }
-        return projectManagementService;
-    }
 
     /**
      * Get FileManagementService.
--- software/caarray.war/src/main/java/gov/nih/nci/caarray/web/delegate/ProjectDelegate.java
+++ software/caarray.war/src/main/java/gov/nih/nci/caarray/web/delegate/ProjectDelegate.java
@@ -1,6 +1,5 @@
 package gov.nih.nci.caarray.web.delegate;
 
-import gov.nih.nci.caarray.application.file.FileManagementService;
 import gov.nih.nci.caarray.application.project.ProjectManagementService;
 import gov.nih.nci.caarray.util.j2ee.ServiceLocator;
 
@@ -13,7 +12,6 @@ public class ProjectDelegate extends BaseDelegate {
 
     private ServiceLocator locator = ServiceLocator.INSTANCE;
     private ProjectManagementService projectManagementService;
-    private FileManagementService fileManagementService;
 
     /**
      * Get ProjectManagementService.
@@ -25,17 +23,6 @@ public class ProjectDelegate extends BaseDelegate {
                 (ProjectManagementService) locator.lookup(ProjectManagementService.JNDI_NAME);
         }
         return projectManagementService;
-    }
-
-    /**
-     * Get FileManagementService.
-     * @return fileManagementService
-     */
-    public FileManagementService getFileManagementService() {
-        if (fileManagementService == null) {
-            fileManagementService = (FileManagementService) locator.lookup(FileManagementService.JNDI_NAME);
-        }
-        return fileManagementService;
     }
 
     /**
