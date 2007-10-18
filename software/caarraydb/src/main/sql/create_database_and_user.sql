def675e78b730f2d432bc7296fb1da552160cdca
Paul Duvall <paul.duvall@stelligent.com>
2007-10-18 14:32:09 +0000

    Modified the % argument to database.server property

--- software/caarraydb/src/main/sql/create_database_and_user.sql
+++ software/caarraydb/src/main/sql/create_database_and_user.sql
@@ -1,4 +1,4 @@
 DROP DATABASE IF EXISTS @database.name@;
 CREATE DATABASE @database.name@;
 
-GRANT ALL ON @database.name@.* TO '@database.user@'@'%' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
+GRANT ALL ON @database.name@.* TO '@database.user@'@'@database.server@' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
