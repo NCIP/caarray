7e345331db867d02f9912eb53bffa613bdb2426b
Paul Duvall <paul.duvall@stelligent.com>
2007-10-18 22:57:41 +0000

    LG: Added DELETE USER statement

--- software/caarraydb/src/main/sql/create_database_and_user.sql
+++ software/caarraydb/src/main/sql/create_database_and_user.sql
@@ -1,5 +1,7 @@
 DROP DATABASE IF EXISTS @database.name@;
 CREATE DATABASE @database.name@;
 
+DELETE FROM mysql.user WHERE User='@database.user@';
+
 GRANT ALL ON @database.name@.* TO '@database.user@'@'localhost' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
 GRANT ALL ON @database.name@.* TO '@database.user@'@'%' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
