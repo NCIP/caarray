43783fafa81c6addfb5bfc9520cafb77d04535da
Paul Duvall <paul.duvall@stelligent.com>
2007-10-18 12:22:07 +0000

    Removed GRANT to localhost

--- software/caarraydb/src/main/sql/create_database_and_user.sql
+++ software/caarraydb/src/main/sql/create_database_and_user.sql
@@ -1,5 +1,4 @@
 DROP DATABASE IF EXISTS @database.name@;
 CREATE DATABASE @database.name@;
 
-GRANT ALL ON @database.name@.* TO '@database.user@'@'localhost' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
 GRANT ALL ON @database.name@.* TO '@database.user@'@'%' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
