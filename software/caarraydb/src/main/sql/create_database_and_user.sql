1c0219c173e89230fc0867edaacca6645b18aaac
Paul Duvall <paul.duvall@stelligent.com>
2007-10-19 16:37:57 +0000

    GRANT SELECT,INSERT,UPDATE,DELETE instead of GRANT ALL

--- software/caarraydb/src/main/sql/create_database_and_user.sql
+++ software/caarraydb/src/main/sql/create_database_and_user.sql
@@ -3,5 +3,5 @@ CREATE DATABASE @database.name@;
 
 DELETE FROM mysql.user WHERE User='@database.user@';
 
-GRANT ALL ON @database.name@.* TO '@database.user@'@'localhost' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
-GRANT ALL ON @database.name@.* TO '@database.user@'@'%' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
+GRANT SELECT,INSERT,UPDATE,DELETE ON @database.name@.* TO '@database.user@'@'localhost' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
+GRANT SELECT,INSERT,UPDATE,DELETE ON @database.name@.* TO '@database.user@'@'%' IDENTIFIED BY '@database.password@' WITH GRANT OPTION;
