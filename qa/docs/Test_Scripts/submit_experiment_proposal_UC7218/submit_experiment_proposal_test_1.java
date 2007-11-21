b95df1a20ca8a86ce7cfbaf6d5ce1a4a6889f7de
keeneron <keeneron@mail.nih.gov>
2007-11-21 15:45:13 +0000

    testing ALPHA 1

--- qa/docs/Test_Scripts/submit_experiment_proposal_UC7218/submit_experiment_proposal_test_1.java
+++ qa/docs/Test_Scripts/submit_experiment_proposal_UC7218/submit_experiment_proposal_test_1.java
@@ -1,53 +1,72 @@
 package submit_experiment_proposal_UC7218;
 
+import base.AbstractSeleniumTest;
 
 
-	import junit.framework.TestCase;
 
-	import com.thoughtworks.selenium.DefaultSelenium;
-	import com.thoughtworks.selenium.Selenium;
+public class submit_experiment_proposal_test_1 extends AbstractSeleniumTest {
+	
 
+//	public void setUp() {
+//		selenium = new DefaultSelenium("localhost", 4444, "*firefox",
+//				"http://array-qa.nci.nih.gov/caarray/");
+//		// *iehta - internet explore experimental browser for https
+//		// *chrome - firefox experimental browser for https
+//		// *firefox - firefox browser
+//		// *iexplore - internet explore browser
+//		selenium.start();
+//	}
 
-	public class submit_experiment_proposal_test_1 extends TestCase {
-	    private Selenium selenium;
-	    public void setUp() {
-	    	selenium = new DefaultSelenium("localhost",
-	            4444, "*iexplore", "http://array-qa.nci.nih.gov/caarray/");
-	    	//*iehta - internet explore experimental browser for https
-	    	//*chrome - firefox experimental browser for https
-	    	//*firefox - firefox browser
-	    	//*iexplore - internet explore browser
-	        selenium.start();
-	    }
-	    
-	    public void testcaarray() throws InterruptedException {
-			selenium.open("/caarray/protected/project/workspace.action");
-			selenium.type("j_username", "caarrayadmin");
-			selenium.type("j_password", "caArray2!");
-			selenium.click("//span/span");
-			selenium.waitForPageToLoad("30000");
-			selenium.click("link=Create/Propose Experiment");
-			Thread.sleep(5000);
-			selenium.type("projectForm_project_experiment_title", "qa experiment");
-			selenium.click("//li/a/span/span");
-			for (int second = 0;; second++) {
-				if (second >= 10) fail("timeout");
-				try { if (selenium.isTextPresent("Project with title qa experiment has been successfully saved.")) break; } catch (Exception e) {}
-				Thread.sleep(1000);
-			}
+	public void testcaarray() throws InterruptedException {
+		loginAsPrincipalInvestigator();
+		Thread.sleep(2000);
 
-			selenium.click("link=My Experiment Workspace");
-			selenium.waitForPageToLoad("30000");
-			for (int second = 0;; second++) {
-				if (second >= 10) fail("timeout");
-				try { if (selenium.isTextPresent("qa experiment")) break; } catch (Exception e) {}
-				Thread.sleep(1000);
-			}
-			
-	    }
-	    
-	    public void tearDown() {
-	        //selenium.stop();
-	    }
+		
+		selenium.click("link=Create/Propose Experiment");
+		Thread.sleep(2000);
+		selenium.type("projectForm_project_experiment_title", "qa experiment");
+		selenium.click("//li/a/span/span");
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Service Type must be set")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Assay Type must be set")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Provider must be set")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("Organism must be set")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("You must select at least one tissue site.")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
+		for (int second = 0;; second++) {
+			if (second >= 60) fail("timeout");
+			try { if (selenium.isTextPresent("You must select at least one tissue type.")) break; } catch (Exception e) {}
+			Thread.sleep(1000);
+		}
+
 	}
 
+	
+	
+	public void tearDown() {
+		// selenium.stop();
+	}
+}
