e1165ec82541f260a51c00c9891044018f95b725
Paul Duvall <paul.duvall@stelligent.com>
2007-12-17 22:36:14 +0000

    Using Ant to generate the flash drive distro

--- tools/installer/create-flash.bat
+++ tools/installer/create-flash.bat
@@ -1,3 +1,1 @@
-copy C:\dev\ncicb\caarray2\tools\installer\install.bat C:\flashdrive
-copy C:\dev\ncicb\caarray2\tools\installer\target\dist\caarray-installer.jar C:\flashdrive
-copy C:\dev\ncicb\caarray2\tools\installer\tools\jdk1.5.0_10.zip C:\flashdrive
+ant -f build-create-flash.xml
