5cfd4b19bc98c01314c0e2a0a6c3bccdbd8b8c89
Eric Tavela <eric.tavela2@nih.gov>
2007-12-20 16:34:00 +0000

    Changed the "tics" in the SQL update

--- software/caarraydb/src/main/sql/update-config-param-stage.sql
+++ software/caarraydb/src/main/sql/update-config-param-stage.sql
@@ -1,2 +1,2 @@
-update config_parameter set raw_value = ‘NCICB@pop.nci.nih.gov’ where param = ‘REG_EMAIL_TO’;
-update config_parameter set raw_value = ‘NCICB@pop.nci.nih.gov’ where param = ‘EMAIL_FROM’;
+update config_parameter set raw_value = 'NCICB@pop.nci.nih.gov' where param = 'REG_EMAIL_TO';
+update config_parameter set raw_value = 'NCICB@pop.nci.nih.gov' where param = 'EMAIL_FROM';
