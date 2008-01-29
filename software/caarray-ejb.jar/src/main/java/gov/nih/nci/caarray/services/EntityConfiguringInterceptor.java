c7d4e94a506367b77dd3e750ec59191014c37220
Todd Parnell <todd.parnell@nih.gov>
2008-01-29 20:53:21 +0000

    checkstyle fix

--- software/caarray-ejb.jar/src/main/java/gov/nih/nci/caarray/services/EntityConfiguringInterceptor.java
+++ software/caarray-ejb.jar/src/main/java/gov/nih/nci/caarray/services/EntityConfiguringInterceptor.java
@@ -113,7 +113,8 @@ public class EntityConfiguringInterceptor {
      * @throws Exception if invoking the method throws an exception.
      */
     @AroundInvoke
-    @SuppressWarnings({ "PMD.SignatureDeclareThrowsException", "unchecked" }) // method invocation wrapper requires throws Exception
+    @SuppressWarnings({ "PMD.SignatureDeclareThrowsException", "unchecked" }) // method invocation wrapper requires
+                                                                              // throws Exception
     public Object prepareReturnValue(InvocationContext invContext) throws Exception {
         // make the call to the underlying method.  This method (prepareReturnValue) wraps the intended method.
         Object returnValue = invContext.proceed();
