abe4f1d29ec51a53fe0f20b54bd06f705bf0c267
Daniel Kokotov <dkokotov@5amsolutions.com>
2008-02-01 21:07:31 +0000

    fix for dev change req 12109. peer review smiller

--- software/caarray.war/src/main/webapp/scripts/global.js
+++ software/caarray.war/src/main/webapp/scripts/global.js
@@ -1,3 +1,11 @@
+// graceful  degradation of firebug's console object
+if (! ("console" in window) || !("firebug" in console)) {
+    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group"
+                 , "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+    window.console = {};
+    for (var i = 0; i <names.length; ++i) window.console[names[i]] = function() {};
+}
+ 
 /* This function is used to change the style class of an element */
 function swapClass(obj, newStyle) {
     obj.className = newStyle;
