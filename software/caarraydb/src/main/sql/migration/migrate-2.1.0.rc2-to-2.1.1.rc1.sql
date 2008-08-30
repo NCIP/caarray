2512bbcaba2d5f71bbc023248bb6c087ec5a4ccd
Steve Lustbader <slustbader@5amsolutions.com>
2008-08-30 03:46:25 +0000

    Improved 2.1.1 upgrade script

--- software/caarraydb/src/main/sql/migration/migrate-2.1.0.rc2-to-2.1.1.rc1.sql
+++ software/caarraydb/src/main/sql/migration/migrate-2.1.0.rc2-to-2.1.1.rc1.sql
@@ -1,4 +1,3 @@
 -- issue 11925
-alter table design_element add column vendor_id varchar(255);
-create index vendor_id_indx on design_element (vendor_id);
+alter table design_element add column vendor_id varchar(255), add index vendor_id_indx (vendor_id);
 alter table array_design_design_file drop index design_file;
