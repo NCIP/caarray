4f92e2522e424615e8db40b5952c5ded03f7eb0f
Doug Harley <dharley@5amsolutions.com>
2008-11-12 21:37:30 +0000

    fixes bugs: [#17568] The caArray installer should set the registration email To: and From: email address in DB. [#17618] The installer should not need any MySQL root user credentials info, nor should it create the DB/connection user. [#17745] csm.application.name is not configurable within caArray GUI installer.
    reviewed by: dan kokotov

--- software/caarraydb/src/main/sql/update-config-param-local-installer.sql
+++ software/caarraydb/src/main/sql/update-config-param-local-installer.sql
@@ -1,3 +1,5 @@
 update config_parameter set raw_value = 'true' where param = 'SEND_CONFIRM_EMAIL';
 update config_parameter set raw_value = 'true' where param = 'SEND_ADMIN_EMAIL';
 update config_parameter set raw_value = 'false' where param = 'DEVELOPMENT_MODE';
+update config_parameter set raw_value = '@user.admin.email.address@' where param = 'REG_EMAIL_TO';
+update config_parameter set raw_value = '@user.admin.email.address@' where param = 'EMAIL_FROM';
