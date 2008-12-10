477b246d01a12756b56e873de58eb735f4e6d51d
Steve Lustbader <slustbader@5amsolutions.com>
2008-12-10 17:55:49 +0000

    Merged 2.2.x branch, rev 8590 - fix for issue 15580, reordering search categories.  peer reviewed on branch

--- software/caarray-common.jar/src/main/java/gov/nih/nci/caarray/domain/search/SearchCategory.java
+++ software/caarray-common.jar/src/main/java/gov/nih/nci/caarray/domain/search/SearchCategory.java
@@ -89,64 +89,56 @@ import org.apache.commons.lang.ArrayUtils;
 
 /**
  * @author Winston Cheng
- *
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 public enum SearchCategory implements ResourceBasedEnum {
     /**
-     * Experiment ID.
-     */
-    EXPERIMENT_ID    ("search.category.experimentId",
-                      new String[]{"p.experiment e"},
-                      "e.publicIdentifier"),
-    /**
      * Experiment title.
      */
-    EXPERIMENT_TITLE ("search.category.experimentTitle",
-                      new String[]{"p.experiment e"},
-                      "e.title"),
+    // NOTE: Experiment title is explicitly set as the first enum here so it shows up first in the UI
+    EXPERIMENT_TITLE("search.category.experimentTitle", new String[] {"p.experiment e"}, "e.title"),
+
+    /**
+     * Experiment ID.
+     */
+    EXPERIMENT_ID("search.category.experimentId", new String[] {"p.experiment e"}, "e.publicIdentifier"),
+
     /**
      * Array provider.
      */
-    ARRAY_PROVIDER   ("search.category.arrayProvider",
-                      new String[]{"p.experiment e", "e.manufacturer m"},
-                      "m.name"),
+    ARRAY_PROVIDER("search.category.arrayProvider", new String[] {"p.experiment e", "e.manufacturer m"}, "m.name"),
+
     /**
      * Array design.
      */
-    ARRAY_DESIGN     ("search.category.arrayDesign",
-                      new String[]{"p.experiment e", "e.arrayDesigns a"},
-                      "a.name"),
+    ARRAY_DESIGN("search.category.arrayDesign", new String[] {"p.experiment e", "e.arrayDesigns a"}, "a.name"),
+
     /**
      * Organism.
      */
-    ORGANISM         ("search.category.organism",
-                      new String[]{"p.experiment e"},
-                      new String[]{"e.organism.commonName", "e.organism.scientificName"}),
+    ORGANISM("search.category.organism", new String[] {"p.experiment e"}, new String[] {"e.organism.commonName",
+            "e.organism.scientificName"}),
     /**
      * Sample.
      */
-    SAMPLE           ("search.category.sample",
-                      new String[]{"p.experiment e", "e.samples s"},
-                      "s.name"),    
+    SAMPLE("search.category.sample", new String[] {"p.experiment e", "e.samples s"}, "s.name"),
+
     /**
      * Disease state.
      */
-    DISEASE_STATE    ("search.category.diseaseState",
-                      new String[]{"p.experiment e"},
-                      ArrayUtils.EMPTY_STRING_ARRAY) {
+    DISEASE_STATE("search.category.diseaseState", new String[] {"p.experiment e"}, ArrayUtils.EMPTY_STRING_ARRAY) {
         /**
          * {@inheritDoc}
          */
         public String getWhereClause() {
             // the subselect is faster when searching by all categories because otherwise we get a huge number of rows
             // with all the joins due to multiple many-to-many associations
-            // it would have been desirable to take this approach for the SAMPLE search category as well, but we cannot 
+            // it would have been desirable to take this approach for the SAMPLE search category as well, but we cannot
             // because of HHH-530, which would cause the security filters to not be applied
             return "e.id in (select exp.id from " + Experiment.class.getName() + " exp left join exp.sources src "
                     + "left join src.diseaseState ds where ds.value like :keyword)";
-        }        
-    };
+       }
+   };
 
     private final String resourceKey;
     private final String[] joins;
@@ -157,29 +149,28 @@ public enum SearchCategory implements ResourceBasedEnum {
         this.resourceKey = resourceKey;
         this.joins = joins;
         this.searchFields = searchFields;
-    }
+   }
 
     /**
-     * @return the resource key that should be used to retrieve a label
-     * for this SearchCategory in the UI
+     * @return the resource key that should be used to retrieve a label for this SearchCategory in the UI
      */
     public String getResourceKey() {
         return this.resourceKey;
-    }
+   }
 
     /**
-     * These are the fields to join against in the HQL query.
-     * Is null if no join is necessary.
+     * These are the fields to join against in the HQL query. Is null if no join is necessary.
+     *
      * @return the fields to join against
      */
     @SuppressWarnings("PMD.MethodReturnsInternalArray")
     public String[] getJoins() {
         return this.joins;
-    }
+   }
 
     /**
-     * @return the where subclause for this search category. this method assumes that the subclause
-     * will be wrapped in parenthesis before being added to the overall where clause of a query. 
+     * @return the where subclause for this search category. this method assumes that the subclause will be wrapped in
+     *         parenthesis before being added to the overall where clause of a query.
      */
     public String getWhereClause() {
         StringBuilder sb = new StringBuilder();
@@ -187,9 +178,9 @@ public enum SearchCategory implements ResourceBasedEnum {
         for (String field : this.searchFields) {
             if (j++ > 0) {
                 sb.append(" OR ");
-            }
+           }
             sb.append(field).append(" LIKE :keyword");
-        }
+       }
         return sb.toString();
-    }
+   }
 }
