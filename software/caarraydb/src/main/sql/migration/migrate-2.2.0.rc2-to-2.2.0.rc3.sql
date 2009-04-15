08379026a0a60e47151f6719530033a4dbbe7bca
Doug Harley <dharley@5amsolutions.com>
2009-04-15 19:34:49 +0000

    fixes bug: [#18755] Fix various problems with UPT administrator email address in caArray installer.
    review: https://reviewboard.5amsolutions.com/r/105/

--- software/caarraydb/src/main/sql/migration/migrate-2.2.0.rc2-to-2.2.0.rc3.sql
+++
@@ -1,3 +0,0 @@
--- issue 17568
-update config_parameter set raw_value = '@user.admin.email.address@' where param = 'REG_EMAIL_TO';
-update config_parameter set raw_value = '@user.admin.email.address@' where param = 'EMAIL_FROM';
--- software/caarraydb/src/main/sql/update-config-param-local-installer.sql
+++ software/caarraydb/src/main/sql/update-config-param-local-installer.sql
@@ -1,5 +1,5 @@
 update config_parameter set raw_value = 'true' where param = 'SEND_CONFIRM_EMAIL';
 update config_parameter set raw_value = 'true' where param = 'SEND_ADMIN_EMAIL';
 update config_parameter set raw_value = 'false' where param = 'DEVELOPMENT_MODE';
-update config_parameter set raw_value = '@user.admin.email.address@' where param = 'REG_EMAIL_TO';
-update config_parameter set raw_value = '@user.admin.email.address@' where param = 'EMAIL_FROM';
+update config_parameter set raw_value = '@user.admin.email.address.to@' where param = 'REG_EMAIL_TO';
+update config_parameter set raw_value = '@user.admin.email.address.from@' where param = 'EMAIL_FROM';
