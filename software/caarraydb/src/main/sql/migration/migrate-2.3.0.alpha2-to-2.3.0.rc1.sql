ff5d973acf9544060424cec6389fe1890dd880f1
gaxzero <gaxzero@mail.nih.gov>
2009-04-21 19:26:05 +0000

    fix upgrade script. https://reviewboard.5amsolutions.com/r/116

--- software/caarraydb/src/main/sql/migration/migrate-2.3.0.alpha2-to-2.3.0.rc1.sql
+++ software/caarraydb/src/main/sql/migration/migrate-2.3.0.alpha2-to-2.3.0.rc1.sql
@@ -49,4 +49,5 @@ alter table array_design drop column assay_type;
 
 -- Implementation of Items 18907 and 19034 - separate project locking, and publication.
 alter table project change column public_id_locked locked bit not null;
+update project set locked = (status = 'PUBLIC');
 alter table project drop column status;
