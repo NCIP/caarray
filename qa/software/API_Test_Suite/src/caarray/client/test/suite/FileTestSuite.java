4cb1d0b9b8073ce36143405e77657f4593486931
Ye Wu <ye.wu@nih.gov>
2009-07-17 16:17:02 +0000

    New test suite for File search

---
+++ qa/software/API_Test_Suite/src/caarray/client/test/suite/FileTestSuite.java
@@ -0,0 +1,151 @@
+/**
+ * 
+ */
+package caarray.client.test.suite;
+
+import gov.nih.nci.caarray.external.v1_0.AbstractCaArrayEntity;
+import gov.nih.nci.caarray.external.v1_0.data.DataFile;
+
+import java.io.File;
+import java.util.List;
+
+import caarray.client.test.ApiFacade;
+import caarray.client.test.TestProperties;
+import caarray.client.test.TestResult;
+import caarray.client.test.search.ExampleSearch;
+import caarray.client.test.search.FileSearch;
+
+/**
+ * Encapsulates a collection of File search-by-example tests.
+ * 
+ * @author Ye Wu
+ *
+ */
+public class FileTestSuite extends SearchByExampleTestSuite
+{
+
+    private static final String CONFIG_FILE = TestProperties.CONFIG_DIR
+    + File.separator + "File.csv";
+
+    private static final String NAME = "Name";
+    
+    private static final String[] COLUMN_HEADERS = new String[] { TEST_CASE,
+        API, NAME, EXPECTED_RESULTS};
+    
+    /**
+     * @param apiFacade
+     */
+    public FileTestSuite(ApiFacade apiFacade)
+    {
+        super(apiFacade);
+    }
+
+    /* (non-Javadoc)
+     * @see caarray.client.test.suite.SearchByExampleTestSuite#evaluateResults(java.util.List, caarray.client.test.search.ExampleSearch, caarray.client.test.TestResult)
+     */
+    @Override
+    protected void evaluateResults(
+            List<? extends AbstractCaArrayEntity> resultsList,
+            ExampleSearch search, TestResult testResult)
+    {
+        FileSearch fileSearch = (FileSearch)search;
+        List<DataFile> fileResults = (List<DataFile>)resultsList;
+        if (fileSearch.getExpectedResults() != null)
+        {
+            int namedResults = 0;
+            for (DataFile fileType : fileResults)
+            {
+                if (fileType.getName() != null)
+                    namedResults++;
+            }
+            if (namedResults != fileSearch.getExpectedResults())
+            {
+                String errorMessage = "Failed with unexpected number of results, expected: "
+                        + fileSearch.getExpectedResults()
+                        + ", actual number of results: " + namedResults;
+                setTestResultFailure(testResult, fileSearch, errorMessage);
+            }
+            else
+            {
+                String detail = "Found expected number of results: "
+                        + namedResults;
+                testResult.addDetail(detail);
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see caarray.client.test.suite.SearchByExampleTestSuite#getExampleSearch()
+     */
+    @Override
+    protected ExampleSearch getExampleSearch()
+    {
+        return new FileSearch();
+    }
+
+    /* (non-Javadoc)
+     * @see caarray.client.test.suite.SearchByExampleTestSuite#populateAdditionalSearchValues(java.lang.String[], caarray.client.test.search.ExampleSearch)
+     */
+    @Override
+    protected void populateAdditionalSearchValues(String[] input,
+            ExampleSearch exampleSearch)
+    {
+    	return;
+    }
+
+    /* (non-Javadoc)
+     * @see caarray.client.test.suite.SearchByExampleTestSuite#populateSearch(java.lang.String[], caarray.client.test.search.ExampleSearch)
+     */
+    @Override
+    protected void populateSearch(String[] input, ExampleSearch exampleSearch)
+    {
+        FileSearch search = (FileSearch)exampleSearch;
+        DataFile example = new DataFile();
+        if (headerIndexMap.get(API) < input.length
+                && !input[headerIndexMap.get(API)].equals(""))
+        {
+            search.setApi(input[headerIndexMap.get(API)].trim());
+        }
+
+        if (headerIndexMap.get(NAME) < input.length && !input[headerIndexMap.get(NAME)].equals(""))
+            example.setName(input[headerIndexMap.get(NAME)].trim());
+        
+        search.setDataFile(example);
+        if (headerIndexMap.get(TEST_CASE) < input.length
+                && !input[headerIndexMap.get(TEST_CASE)].equals(""))
+            search.setTestCase(Float.parseFloat(input[headerIndexMap.get(TEST_CASE)]
+                    .trim()));
+        if (headerIndexMap.get(EXPECTED_RESULTS) < input.length
+                && !input[headerIndexMap.get(EXPECTED_RESULTS)].equals(""))
+            search.setExpectedResults(Integer
+                    .parseInt(input[headerIndexMap.get(EXPECTED_RESULTS)].trim()));
+    }
+
+    /* (non-Javadoc)
+     * @see caarray.client.test.suite.ConfigurableTestSuite#getColumnHeaders()
+     */
+    @Override
+    protected String[] getColumnHeaders()
+    {
+        return COLUMN_HEADERS;
+    }
+
+    /* (non-Javadoc)
+     * @see caarray.client.test.suite.ConfigurableTestSuite#getConfigFilename()
+     */
+    @Override
+    protected String getConfigFilename()
+    {
+        return CONFIG_FILE;
+    }
+
+    /* (non-Javadoc)
+     * @see caarray.client.test.suite.ConfigurableTestSuite#getType()
+     */
+    @Override
+    protected String getType()
+    {
+        return "FileType";
+    }
+
+}
