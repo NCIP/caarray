4a69d36316322be99a38c55cafeacbf6f967ac4a
Daniel Kokotov <dkokotov@5amsolutions.com>
2009-09-25 19:33:46 +0000

    upped file retrieval chunk size to 128k, as it seems to be the optimum

--- software/caarraydb/src/main/sql/migration/migrate-2.3.0.rc5-to-2.3.0.rc7.sql
+++ software/caarraydb/src/main/sql/migration/migrate-2.3.0.rc5-to-2.3.0.rc7.sql
@@ -1,2 +1,2 @@
 -- GF 23526
-insert into config_parameter (param, raw_value) values ('FILE_RETRIEVAL_API_CHUNK_SIZE', '65536');
+insert into config_parameter (param, raw_value) values ('FILE_RETRIEVAL_API_CHUNK_SIZE', '131072');
