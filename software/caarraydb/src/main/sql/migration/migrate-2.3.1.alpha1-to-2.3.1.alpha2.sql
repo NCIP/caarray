27e25858b2a8c202d2abe962d187f9f95bd300ec
gaxzero <gaxzero@mail.nih.gov>
2009-12-01 21:38:43 +0000

    fix missing geo accession for QA nightly deployment, GF 20295, no review

--- software/caarraydb/src/main/sql/migration/migrate-2.3.1.alpha1-to-2.3.1.alpha2.sql
+++ software/caarraydb/src/main/sql/migration/migrate-2.3.1.alpha1-to-2.3.1.alpha2.sql
@@ -1,5 +1,4 @@
 -- GEO accession updates from https://gforge.nci.nih.gov/svnroot/caarray2/trunk/docs/requirements/use_cases/user_stories/caArray_GEO_platforms_reviewed.xls
-update array_design set geo_accession = 'GPL3921' where name = 'HT-HG_U133A' and provider = (select id from contact where provider = true and name = 'Affymetrix');
 update array_design set geo_accession = 'GPL6244' where name = 'HuGene-1_0-st-v1.r3' and provider = (select id from contact where provider = true and name = 'Affymetrix');
 update array_design set geo_accession = 'GPL2641' where name = 'Mapping10K_Xba142' and provider = (select id from contact where provider = true and name = 'Affymetrix');
 update array_design set geo_accession = 'GPL2004' where name = 'Mapping50K_Hind240' and provider = (select id from contact where provider = true and name = 'Affymetrix');
@@ -9,4 +8,3 @@ update array_design set geo_accession = 'GPL5188' where name = 'HuEx-1_0-st-v1' 
 update array_design set geo_accession = 'GPL341' where name = 'RAE230A' and provider = (select id from contact where provider = true and name = 'Affymetrix');
 update array_design set geo_accession = 'GPL1355' where name = 'Rat230_2' and provider = (select id from contact where provider = true and name = 'Affymetrix');
 update array_design set geo_accession = 'GPL98' where name = 'Hu35KsubA' and provider = (select id from contact where provider = true and name = 'Affymetrix');
-update array_design set geo_accession = 'GPL3738' where name = 'Canine_2' and provider = (select id from contact where provider = true and name = 'Affymetrix');
