c567e23211307a8c725fd6c4afbad3f1bc8bf4aa
scottj <scottj@mail.nih.gov>
2010-06-07 20:38:39 +0000

    Gax's fix to the term_source trigger

--- software/caarraydb/src/main/sql/migration/migrate-2.3.1.alpha2-20295-to-2.3.1.2-28739.sql
+++ software/caarraydb/src/main/sql/migration/migrate-2.3.1.alpha2-20295-to-2.3.1.2-28739.sql
@@ -1,12 +1,9 @@
 -- GF 28739 revert standard term source names
 
-DELIMITER |
-
 CREATE TRIGGER rename_standard_term_sources BEFORE UPDATE ON term_source FOR EACH ROW
 BEGIN
 IF OLD.name = 'MO' THEN SET NEW.name = 'MO'; END IF;
 IF OLD.name = 'caArray' THEN SET NEW.name = 'caArray'; END IF;
 END;
-|
 
-DELIMITER ;
+~~the end~~
