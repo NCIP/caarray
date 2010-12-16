a05fe37ca964b895b4099904e9c86a7952c875f6
Doug Harley <dharley@5amsolutions.com>
2010-12-16 17:34:00 +0000

    fixes registration email after upgrade

--- software/caarraydb/src/main/sql/liquibase/db-upgrade/mysql/env-specific/local-installer.sql
+++ software/caarraydb/src/main/sql/liquibase/db-upgrade/mysql/env-specific/local-installer.sql
@@ -1,5 +1,5 @@
 update config_parameter set raw_value = 'true' where param = 'SEND_CONFIRM_EMAIL';
 update config_parameter set raw_value = 'true' where param = 'SEND_ADMIN_EMAIL';
 update config_parameter set raw_value = 'false' where param = 'DEVELOPMENT_MODE';
-update config_parameter set raw_value = '@user.admin.email.address.to@' where param = 'REG_EMAIL_TO';
-update config_parameter set raw_value = '@user.admin.email.address.from@' where param = 'EMAIL_FROM';
+update config_parameter set raw_value = '@user.admin.email.address.to@' where param = 'REG_EMAIL_TO' and '@user.admin.email.address.to@' not like '@%@';
+update config_parameter set raw_value = '@user.admin.email.address.from@' where param = 'EMAIL_FROM' and '@user.admin.email.address.from@' not like '@%@';
