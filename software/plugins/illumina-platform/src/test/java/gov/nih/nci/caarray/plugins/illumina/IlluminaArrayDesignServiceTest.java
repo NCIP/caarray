cdb6103a85592f35c4788799aba81cac5b077d7d
Krishna Kanchinadam <kkanchinadam@5amsolutions.com>
2012-01-13 15:41:16 +0000

    Array-2240: Reduce ci time for IlluminaArrayDesignServiceTest.java. reduced ci time by 8 seconds. Review: CR-CAARRAY-207

--- software/plugins/illumina-platform/src/test/java/gov/nih/nci/caarray/plugins/illumina/IlluminaArrayDesignServiceTest.java
+++ software/plugins/illumina-platform/src/test/java/gov/nih/nci/caarray/plugins/illumina/IlluminaArrayDesignServiceTest.java
@@ -1,83 +1,83 @@
 /**
  * The software subject to this notice and license includes both human readable
  * source code form and machine readable, binary, object code form. The caArray2
- * Software was developed in conjunction with the National Cancer Institute 
- * (NCI) by NCI employees and 5AM Solutions, Inc. (5AM). To the extent 
- * government employees are authors, any rights in such works shall be subject 
- * to Title 17 of the United States Code, section 105. 
+ * Software was developed in conjunction with the National Cancer Institute
+ * (NCI) by NCI employees and 5AM Solutions, Inc. (5AM). To the extent
+ * government employees are authors, any rights in such works shall be subject
+ * to Title 17 of the United States Code, section 105.
  *
- * This caArray2 Software License (the License) is between NCI and You. You (or 
- * Your) shall mean a person or an entity, and all other entities that control, 
- * are controlled by, or are under common control with the entity. Control for 
- * purposes of this definition means (i) the direct or indirect power to cause 
+ * This caArray2 Software License (the License) is between NCI and You. You (or
+ * Your) shall mean a person or an entity, and all other entities that control,
+ * are controlled by, or are under common control with the entity. Control for
+ * purposes of this definition means (i) the direct or indirect power to cause
  * the direction or management of such entity, whether by contract or otherwise,
- * or (ii) ownership of fifty percent (50%) or more of the outstanding shares, 
- * or (iii) beneficial ownership of such entity. 
+ * or (ii) ownership of fifty percent (50%) or more of the outstanding shares,
+ * or (iii) beneficial ownership of such entity.
  *
- * This License is granted provided that You agree to the conditions described 
- * below. NCI grants You a non-exclusive, worldwide, perpetual, fully-paid-up, 
- * no-charge, irrevocable, transferable and royalty-free right and license in 
- * its rights in the caArray2 Software to (i) use, install, access, operate, 
+ * This License is granted provided that You agree to the conditions described
+ * below. NCI grants You a non-exclusive, worldwide, perpetual, fully-paid-up,
+ * no-charge, irrevocable, transferable and royalty-free right and license in
+ * its rights in the caArray2 Software to (i) use, install, access, operate,
  * execute, copy, modify, translate, market, publicly display, publicly perform,
- * and prepare derivative works of the caArray2 Software; (ii) distribute and 
- * have distributed to and by third parties the caArray2 Software and any 
- * modifications and derivative works thereof; and (iii) sublicense the 
- * foregoing rights set out in (i) and (ii) to third parties, including the 
- * right to license such rights to further third parties. For sake of clarity, 
- * and not by way of limitation, NCI shall have no right of accounting or right 
- * of payment from You or Your sub-licensees for the rights granted under this 
+ * and prepare derivative works of the caArray2 Software; (ii) distribute and
+ * have distributed to and by third parties the caArray2 Software and any
+ * modifications and derivative works thereof; and (iii) sublicense the
+ * foregoing rights set out in (i) and (ii) to third parties, including the
+ * right to license such rights to further third parties. For sake of clarity,
+ * and not by way of limitation, NCI shall have no right of accounting or right
+ * of payment from You or Your sub-licensees for the rights granted under this
  * License. This License is granted at no charge to You.
  *
- * Your redistributions of the source code for the Software must retain the 
- * above copyright notice, this list of conditions and the disclaimer and 
- * limitation of liability of Article 6, below. Your redistributions in object 
- * code form must reproduce the above copyright notice, this list of conditions 
- * and the disclaimer of Article 6 in the documentation and/or other materials 
- * provided with the distribution, if any. 
+ * Your redistributions of the source code for the Software must retain the
+ * above copyright notice, this list of conditions and the disclaimer and
+ * limitation of liability of Article 6, below. Your redistributions in object
+ * code form must reproduce the above copyright notice, this list of conditions
+ * and the disclaimer of Article 6 in the documentation and/or other materials
+ * provided with the distribution, if any.
  *
- * Your end-user documentation included with the redistribution, if any, must 
- * include the following acknowledgment: This product includes software 
- * developed by 5AM and the National Cancer Institute. If You do not include 
- * such end-user documentation, You shall include this acknowledgment in the 
+ * Your end-user documentation included with the redistribution, if any, must
+ * include the following acknowledgment: This product includes software
+ * developed by 5AM and the National Cancer Institute. If You do not include
+ * such end-user documentation, You shall include this acknowledgment in the
  * Software itself, wherever such third-party acknowledgments normally appear.
  *
- * You may not use the names "The National Cancer Institute", "NCI", or "5AM" 
- * to endorse or promote products derived from this Software. This License does 
+ * You may not use the names "The National Cancer Institute", "NCI", or "5AM"
+ * to endorse or promote products derived from this Software. This License does
  * not authorize You to use any trademarks, service marks, trade names, logos or
- * product names of either NCI or 5AM, except as required to comply with the 
- * terms of this License. 
+ * product names of either NCI or 5AM, except as required to comply with the
+ * terms of this License.
  *
- * For sake of clarity, and not by way of limitation, You may incorporate this 
- * Software into Your proprietary programs and into any third party proprietary 
- * programs. However, if You incorporate the Software into third party 
+ * For sake of clarity, and not by way of limitation, You may incorporate this
+ * Software into Your proprietary programs and into any third party proprietary
+ * programs. However, if You incorporate the Software into third party
  * proprietary programs, You agree that You are solely responsible for obtaining
- * any permission from such third parties required to incorporate the Software 
- * into such third party proprietary programs and for informing Your 
- * sub-licensees, including without limitation Your end-users, of their 
- * obligation to secure any required permissions from such third parties before 
- * incorporating the Software into such third party proprietary software 
- * programs. In the event that You fail to obtain such permissions, You agree 
- * to indemnify NCI for any claims against NCI by such third parties, except to 
- * the extent prohibited by law, resulting from Your failure to obtain such 
- * permissions. 
+ * any permission from such third parties required to incorporate the Software
+ * into such third party proprietary programs and for informing Your
+ * sub-licensees, including without limitation Your end-users, of their
+ * obligation to secure any required permissions from such third parties before
+ * incorporating the Software into such third party proprietary software
+ * programs. In the event that You fail to obtain such permissions, You agree
+ * to indemnify NCI for any claims against NCI by such third parties, except to
+ * the extent prohibited by law, resulting from Your failure to obtain such
+ * permissions.
  *
- * For sake of clarity, and not by way of limitation, You may add Your own 
- * copyright statement to Your modifications and to the derivative works, and 
- * You may provide additional or different license terms and conditions in Your 
- * sublicenses of modifications of the Software, or any derivative works of the 
+ * For sake of clarity, and not by way of limitation, You may add Your own
+ * copyright statement to Your modifications and to the derivative works, and
+ * You may provide additional or different license terms and conditions in Your
+ * sublicenses of modifications of the Software, or any derivative works of the
  * Software as a whole, provided Your use, reproduction, and distribution of the
  * Work otherwise complies with the conditions stated in this License.
  *
- * THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES, 
- * (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, 
- * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO 
- * EVENT SHALL THE NATIONAL CANCER INSTITUTE, 5AM SOLUTIONS, INC. OR THEIR 
- * AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE NATIONAL CANCER INSTITUTE, 5AM SOLUTIONS, INC. OR THEIR
+ * AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package gov.nih.nci.caarray.plugins.illumina;
@@ -104,7 +104,7 @@ import org.junit.Test;
 /**
  * Illumina-specific tests of ArrayDesignService. Temporary - need to factor our to tests independent of
  * ArrayDesignService.
- * 
+ *
  * @author dkokotov
  */
 public class IlluminaArrayDesignServiceTest extends AbstractArrayDesignServiceTest {
@@ -115,65 +115,68 @@ public class IlluminaArrayDesignServiceTest extends AbstractArrayDesignServiceTe
 
     @Test
     public void testValidateDesign_IlluminaHumanHap300() {
-        final CaArrayFile designFile = getIlluminaCaArrayFile(IlluminaArrayDesignFiles.HUMAN_HAP_300_CSV);
-        final ValidationResult result = this.arrayDesignService.validateDesign(Collections.singleton(designFile));
-        assertTrue(result.isValid());
+        this.testValidDesign(IlluminaArrayDesignFiles.HUMAN_HAP_300_CSV);
     }
 
     @Test
     public void testValidateDesign_IlluminaHumanWG6() {
-        CaArrayFile designFile = getIlluminaCaArrayFile(IlluminaArrayDesignFiles.HUMAN_WG6_CSV);
-        ValidationResult result = this.arrayDesignService.validateDesign(Collections.singleton(designFile));
-        assertTrue(result.isValid());
+        this.testValidDesign(IlluminaArrayDesignFiles.HUMAN_WG6_CSV);
+        this.testInValidDesign(IlluminaArrayDesignFiles.HUMAN_WG6_CSV_INVALID_HEADERS, true);
+        this.testInValidDesign(IlluminaArrayDesignFiles.HUMAN_WG6_CSV_INVALID_CONTENT, false);
+        this.testInValidDesign(AffymetrixArrayDataFiles.TEST3_CEL, false);
+    }
 
-        designFile = getIlluminaCaArrayFile(IlluminaArrayDesignFiles.HUMAN_WG6_CSV_INVALID_HEADERS);
+    private void testValidDesign(File file) {
+        this.testValidOrInvalidDesgin(file, true, false);
+    }
 
-        result = this.arrayDesignService.validateDesign(Collections.singleton(designFile));
-        assertFalse(result.isValid());
-        assertEquals(1, result.getFileValidationResults().size());
-        assertEquals("Unable to read file", result.getFileValidationResults().get(0).getMessages().get(0).getMessage());
-        assertEquals("Unable to read file", designFile.getValidationResult().getMessages().get(0).getMessage());
+    private void testInValidDesign(File file, boolean assertValidationResultMessages) {
+        this.testValidOrInvalidDesgin(file, false, assertValidationResultMessages);
+    }
 
-        designFile = getIlluminaCaArrayFile(IlluminaArrayDesignFiles.HUMAN_WG6_CSV_INVALID_CONTENT);
-        result = this.arrayDesignService.validateDesign(Collections.singleton(designFile));
-        assertFalse(result.isValid());
+    private void testValidOrInvalidDesgin(File file, boolean isValid, boolean assertValidationResultMessages) {
+        final CaArrayFile designFile = getIlluminaCaArrayFile(file);
+        final ValidationResult result = this.arrayDesignService.validateDesign(Collections.singleton(designFile));
+        if (isValid) {
+            assertTrue(result.isValid());
+        } else {
+            assertFalse(result.isValid());
+        }
 
-        designFile = getIlluminaCaArrayFile(AffymetrixArrayDataFiles.TEST3_CEL);
-        result = this.arrayDesignService.validateDesign(Collections.singleton(designFile));
-        assertFalse(result.isValid());
+        if (assertValidationResultMessages) {
+            assertEquals(1, result.getFileValidationResults().size());
+            assertEquals("Unable to read file", result.getFileValidationResults().get(0).getMessages().get(0).getMessage());
+            assertEquals("Unable to read file", designFile.getValidationResult().getMessages().get(0).getMessage());
+        }
     }
 
     @Test
-    public void testImportDesignDetails_IlluminaHumanWG6() {
-        final CaArrayFile designFile = getIlluminaCaArrayFile(IlluminaArrayDesignFiles.HUMAN_WG6_CSV);
+    public void testImportDesignAndDetails_IlluminaHumanHap300() {
+        this.testImportDesignAndDetails(IlluminaArrayDesignFiles.HUMAN_HAP_300_CSV, "HumanHap300v2_A", "illumina.com",
+                "PhysicalArrayDesign", "HumanHap300v2_A", 318237, 318237);
+    }
+
+    @Test
+    public void testImportDesignAndDetails_IlluminaHumanWG6() {
+        this.testImportDesignAndDetails(IlluminaArrayDesignFiles.HUMAN_WG6_CSV, "Human_WG-6", "illumina.com",
+                "PhysicalArrayDesign", "Human_WG-6", 47296, 47296);
+    }
+
+    private void testImportDesignAndDetails(File file, String name, String lsidAuthority, String lsidNamespace,
+            String lsidObjectId, int expectedFeatures, int expectedProbes) {
+        final CaArrayFile designFile = getIlluminaCaArrayFile(file);
         final ArrayDesign arrayDesign = new ArrayDesign();
         arrayDesign.addDesignFile(designFile);
         this.arrayDesignService.importDesign(arrayDesign);
         this.arrayDesignService.importDesignDetails(arrayDesign);
-        assertEquals(47296, arrayDesign.getDesignDetails().getProbes().size());
-    }
 
-    @Test
-    public void testImportDesign_IlluminaHumanHap300() {
-        final CaArrayFile designFile = getIlluminaCaArrayFile(IlluminaArrayDesignFiles.HUMAN_HAP_300_CSV);
-        final ArrayDesign arrayDesign = new ArrayDesign();
-        arrayDesign.addDesignFile(designFile);
-        this.arrayDesignService.importDesign(arrayDesign);
-        assertEquals("HumanHap300v2_A", arrayDesign.getName());
-        assertEquals("illumina.com", arrayDesign.getLsidAuthority());
-        assertEquals("PhysicalArrayDesign", arrayDesign.getLsidNamespace());
-        assertEquals("HumanHap300v2_A", arrayDesign.getLsidObjectId());
-        assertEquals(318237, arrayDesign.getNumberOfFeatures().intValue());
-    }
+        assertEquals(name, arrayDesign.getName());
+        assertEquals(lsidAuthority, arrayDesign.getLsidAuthority());
+        assertEquals(lsidNamespace, arrayDesign.getLsidNamespace());
+        assertEquals(lsidObjectId, arrayDesign.getLsidObjectId());
+        assertEquals(expectedFeatures, arrayDesign.getNumberOfFeatures().intValue());
+        assertEquals(expectedProbes, arrayDesign.getDesignDetails().getProbes().size());
 
-    @Test
-    public void testImportDesignDetails_IlluminaHumanHap300() {
-        final CaArrayFile designFile = getIlluminaCaArrayFile(IlluminaArrayDesignFiles.HUMAN_HAP_300_CSV);
-        final ArrayDesign arrayDesign = new ArrayDesign();
-        arrayDesign.addDesignFile(designFile);
-        this.arrayDesignService.importDesign(arrayDesign);
-        this.arrayDesignService.importDesignDetails(arrayDesign);
-        assertEquals(318237, arrayDesign.getDesignDetails().getProbes().size());
         for (final LogicalProbe probe : arrayDesign.getDesignDetails().getLogicalProbes()) {
             assertNotNull(probe);
             assertNotNull(probe.getAnnotation());
@@ -186,19 +189,6 @@ public class IlluminaArrayDesignServiceTest extends AbstractArrayDesignServiceTe
             assertEquals(probe.getName(), annotation.getDbSNPId());
             assertEquals(arrayDesign.getDesignDetails(), probe.getArrayDesignDetails());
         }
-    }
-
-    @Test
-    public void testImportDesign_IlluminaHumanWG6() {
-        final CaArrayFile designFile = getIlluminaCaArrayFile(IlluminaArrayDesignFiles.HUMAN_WG6_CSV);
-        final ArrayDesign arrayDesign = new ArrayDesign();
-        arrayDesign.addDesignFile(designFile);
-        this.arrayDesignService.importDesign(arrayDesign);
-        assertEquals("Human_WG-6", arrayDesign.getName());
-        assertEquals("illumina.com", arrayDesign.getLsidAuthority());
-        assertEquals("PhysicalArrayDesign", arrayDesign.getLsidNamespace());
-        assertEquals("Human_WG-6", arrayDesign.getLsidObjectId());
-        assertEquals(47296, arrayDesign.getNumberOfFeatures().intValue());
     }
 
     private CaArrayFile getIlluminaCaArrayFile(final File file) {
