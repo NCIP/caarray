9e25d24787cc38acd7f84042102967c664bb328c
Todd Parnell <todd.parnell@nih.gov>
2012-01-24 21:16:01 +0000

    ARRAY-2238: reduce illumina file import test time.  Peer review CR-CAARRAY-220 KK and Paul.

--- software/plugins/illumina-platform/src/test/java/gov/nih/nci/caarray/plugins/illumina/IlluminaFileImportIntegrationTest.java
+++ software/plugins/illumina-platform/src/test/java/gov/nih/nci/caarray/plugins/illumina/IlluminaFileImportIntegrationTest.java
@@ -103,10 +103,12 @@ import gov.nih.nci.caarray.test.data.arraydesign.IlluminaArrayDesignFiles;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.hibernate.Transaction;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -122,12 +124,66 @@ public class IlluminaFileImportIntegrationTest extends AbstractFileManagementSer
     }
 
     @Test
+    @Ignore("Large test takes >1min, does not add to coverage.")
     public void testIlluminaCsvDataImport() throws Exception {
-        final ParsedDataPersister pdp = this.injector.getInstance(ParsedDataPersister.class);
+        importDesignAndDataFilesIntoProject(IlluminaArrayDesignFiles.HUMAN_WG6_CSV, 
+                CsvDataHandler.DATA_CSV_FILE_TYPE, IlluminaArrayDataFiles.HUMAN_WG6_SMALL);
+        
+        final List<String> intColumns =
+                new ArrayList<String>(Arrays.asList(SampleProbeProfileQuantitationType.AVG_NBEADS.getName()));
+        final List<String> floatColumns =
+                new ArrayList<String>(Arrays.asList(SampleProbeProfileQuantitationType.AVG_SIGNAL.getName(),
+                        SampleProbeProfileQuantitationType.BEAD_STDEV.getName(),
+                        SampleProbeProfileQuantitationType.DETECTION.getName()));
 
-        final int numberOfProbes = 10;
-        importDesignAndDataFilesIntoProject(IlluminaArrayDesignFiles.HUMAN_WG6_CSV, CsvDataHandler.DATA_CSV_FILE_TYPE,
-                IlluminaArrayDataFiles.HUMAN_WG6_SMALL);
+        assertImportCorrect(10, 19, 4, intColumns, floatColumns, Collections.<String>emptyList());
+    }
+
+    @Test
+    public void testIlluminaSampleProbeProfileImport() throws Exception {
+        importDesignAndDataFilesIntoProject(IlluminaArrayDesignFiles.MOUSE_REF_8,
+                SampleProbeProfileHandler.SAMPLE_PROBE_PROFILE_FILE_TYPE, IlluminaArrayDataFiles.SAMPLE_PROBE_PROFILE);
+        
+        final List<String> intColumns =
+                new ArrayList<String>(Arrays.asList(SampleProbeProfileQuantitationType.NARRAYS.getName(),
+                        SampleProbeProfileQuantitationType.AVG_NBEADS.getName()));
+            final List<String> floatColumns =
+                new ArrayList<String>(Arrays.asList(SampleProbeProfileQuantitationType.MIN_SIGNAL.getName(),
+                        SampleProbeProfileQuantitationType.AVG_SIGNAL.getName(),
+                        SampleProbeProfileQuantitationType.MAX_SIGNAL.getName(),
+                        SampleProbeProfileQuantitationType.ARRAY_STDEV.getName(),
+                        SampleProbeProfileQuantitationType.BEAD_STDEV.getName(),
+                        SampleProbeProfileQuantitationType.DETECTION.getName()));
+
+        assertImportCorrect(3, 16, 8, intColumns, floatColumns, Collections.<String>emptyList());
+    }
+
+    @Test
+    public void testIlluminaGenotypingProcessedMatrixImport() throws Exception {
+        Transaction tx = this.hibernateHelper.beginTransaction();
+        ArrayDesign design = IlluminaGenotypingProcessedMatrixHandlerTest.buildArrayDesign();
+        this.hibernateHelper.getCurrentSession().save(design.getAssayTypes().iterator().next());
+        this.hibernateHelper.getCurrentSession().save(design);
+        tx.commit();
+
+        importDataFilesIntoProject(design, GenotypingProcessedMatrixHandler.GENOTYPING_MATRIX_FILE_TYPE,
+                IlluminaArrayDataFiles.ILLUMINA_DERIVED_1_HYB);
+        
+        final List<String> stringColumns =
+                new ArrayList<String>(Arrays.asList(IlluminaGenotypingProcessedMatrixQuantitationType.ALLELE.getName()));
+        final List<String> floatColumns =
+                new ArrayList<String>(Arrays.asList(
+                        IlluminaGenotypingProcessedMatrixQuantitationType.B_ALLELE_FREQ.getName(),
+                        IlluminaGenotypingProcessedMatrixQuantitationType.GC_SCORE.getName(),
+                        IlluminaGenotypingProcessedMatrixQuantitationType.LOG_R_RATIO.getName(),
+                        IlluminaGenotypingProcessedMatrixQuantitationType.R.getName(),
+                        IlluminaGenotypingProcessedMatrixQuantitationType.THETA.getName()));
+        assertImportCorrect(3, 1, 6, Collections.<String>emptyList(), floatColumns, stringColumns);
+    }
+
+    private void assertImportCorrect(int numberOfProbes, int hybridizationDataListSize,
+            int hybridizationColumns, List<String> intColumns, List<String> floatColumns, List<String> stringColumns) {
+        final ParsedDataPersister pdp = this.injector.getInstance(ParsedDataPersister.class);
         final Transaction tx = this.hibernateHelper.beginTransaction();
         final Project project = getTestProject();
         final ArrayDesign design = project.getExperiment().getArrayDesigns().iterator().next();
@@ -142,140 +198,37 @@ public class IlluminaFileImportIntegrationTest extends AbstractFileManagementSer
         }
         final List<HybridizationData> hdl =
             hyb.getDerivedDataCollection().iterator().next().getDataSet().getHybridizationDataList();
-        assertEquals(19, hdl.size());
+        assertEquals(hybridizationDataListSize, hdl.size());
         for (final HybridizationData hd : hdl) {
-            assertEquals(4, hd.getColumns().size());
-            final List<String> intColumns =
-                new ArrayList<String>(Arrays.asList(SampleProbeProfileQuantitationType.AVG_NBEADS.getName()));
-            final List<String> floatColumns =
-                new ArrayList<String>(Arrays.asList(SampleProbeProfileQuantitationType.AVG_SIGNAL.getName(),
-                        SampleProbeProfileQuantitationType.BEAD_STDEV.getName(),
-                        SampleProbeProfileQuantitationType.DETECTION.getName()));
+            List<String> hybIntColumns = new ArrayList<String>();
+            List<String> hybFloatColumns = new ArrayList<String>();
+            List<String> hybStringColumns = new ArrayList<String>();
+            hybIntColumns.addAll(intColumns);
+            hybFloatColumns.addAll(floatColumns);
+            hybStringColumns.addAll(stringColumns);
+            assertEquals(hybridizationColumns, hd.getColumns().size());
             for (final AbstractDataColumn c : hd.getColumns()) {
                 pdp.loadFromStorage(c);
                 final String name = c.getQuantitationType().getName();
-                if (intColumns.contains(name)) {
+                if (hybIntColumns.contains(name)) {
                     assertEquals(numberOfProbes, ((IntegerColumn) c).getValues().length);
                     assertTrue("missing " + c.getQuantitationType(),
-                            intColumns.remove(c.getQuantitationType().getName()));
-                } else if (floatColumns.contains(name)) {
+                            hybIntColumns.remove(c.getQuantitationType().getName()));
+                } else if (hybFloatColumns.contains(name)) {
                     assertEquals(numberOfProbes, ((FloatColumn) c).getValues().length);
                     assertTrue("missing " + c.getQuantitationType(),
-                            floatColumns.remove(c.getQuantitationType().getName()));
+                            hybFloatColumns.remove(c.getQuantitationType().getName()));
+                } else if (hybStringColumns.contains(name)) {
+                    assertEquals(numberOfProbes, ((StringColumn) c).getValues().length);
+                    assertTrue("missing " + c.getQuantitationType(),
+                            hybStringColumns.remove(c.getQuantitationType().getName()));
                 } else {
                     fail("unexpected column: " + name);
                 }
             }
-            assertTrue("not all columns present", intColumns.isEmpty() && floatColumns.isEmpty());
+            assertTrue("not all columns present", hybIntColumns.isEmpty() 
+                    && hybFloatColumns.isEmpty() && hybStringColumns.isEmpty());
         }
-        tx.commit();
-    }
-
-    @Test
-    public void testIlluminaSampleProbeProfileImport() throws Exception {
-        final ParsedDataPersister pdp = this.injector.getInstance(ParsedDataPersister.class);
-
-        importDesignAndDataFilesIntoProject(IlluminaArrayDesignFiles.MOUSE_REF_8,
-                SampleProbeProfileHandler.SAMPLE_PROBE_PROFILE_FILE_TYPE, IlluminaArrayDataFiles.SAMPLE_PROBE_PROFILE);
-        final Transaction tx = this.hibernateHelper.beginTransaction();
-        final Project project = getTestProject();
-        final ArrayDesign design = project.getExperiment().getArrayDesigns().iterator().next();
-        final Hybridization hyb = project.getExperiment().getHybridizations().iterator().next();
-        final List<AbstractDesignElement> l =
-            hyb.getDerivedDataCollection().iterator().next().getDataSet().getDesignElementList()
-            .getDesignElements();
-        assertEquals(3, l.size());
-        for (final AbstractDesignElement de : l) {
-            final PhysicalProbe p = (PhysicalProbe) de;
-            assertTrue(design.getDesignDetails().getProbes().contains(p));
-        }
-        final List<HybridizationData> hdl =
-            hyb.getDerivedDataCollection().iterator().next().getDataSet().getHybridizationDataList();
-        assertEquals(16, hdl.size());
-        for (final HybridizationData hd : hdl) {
-            assertEquals(8, hd.getColumns().size());
-            final List<String> intColumns =
-                new ArrayList<String>(Arrays.asList(SampleProbeProfileQuantitationType.NARRAYS.getName(),
-                        SampleProbeProfileQuantitationType.AVG_NBEADS.getName()));
-            final List<String> floatColumns =
-                new ArrayList<String>(Arrays.asList(SampleProbeProfileQuantitationType.MIN_SIGNAL.getName(),
-                        SampleProbeProfileQuantitationType.AVG_SIGNAL.getName(),
-                        SampleProbeProfileQuantitationType.MAX_SIGNAL.getName(),
-                        SampleProbeProfileQuantitationType.ARRAY_STDEV.getName(),
-                        SampleProbeProfileQuantitationType.BEAD_STDEV.getName(),
-                        SampleProbeProfileQuantitationType.DETECTION.getName()));
-            for (final AbstractDataColumn c : hd.getColumns()) {
-                pdp.loadFromStorage(c);
-                final String name = c.getQuantitationType().getName();
-                if (intColumns.contains(name)) {
-                    assertEquals(3, ((IntegerColumn) c).getValues().length);
-                    assertTrue("missing " + c.getQuantitationType(),
-                            intColumns.remove(c.getQuantitationType().getName()));
-                } else if (floatColumns.contains(name)) {
-                    assertEquals(3, ((FloatColumn) c).getValues().length);
-                    assertTrue("missing " + c.getQuantitationType(),
-                            floatColumns.remove(c.getQuantitationType().getName()));
-                } else {
-                    fail("unexpected column: " + name);
-                }
-            }
-            assertTrue("not all columns present", intColumns.isEmpty() && floatColumns.isEmpty());
-        }
-        tx.commit();
-    }
-
-    @Test
-    public void testIlluminaGenotypingProcessedMatrixImport() throws Exception {
-        final ParsedDataPersister pdp = this.injector.getInstance(ParsedDataPersister.class);
-
-        Transaction tx = this.hibernateHelper.beginTransaction();
-        ArrayDesign design = IlluminaGenotypingProcessedMatrixHandlerTest.buildArrayDesign();
-        this.hibernateHelper.getCurrentSession().save(design.getAssayTypes().iterator().next());
-        this.hibernateHelper.getCurrentSession().save(design);
-        tx.commit();
-
-        importDataFilesIntoProject(design, GenotypingProcessedMatrixHandler.GENOTYPING_MATRIX_FILE_TYPE,
-                IlluminaArrayDataFiles.ILLUMINA_DERIVED_1_HYB);
-        tx = this.hibernateHelper.beginTransaction();
-        final Project project = getTestProject();
-        design = (ArrayDesign) this.hibernateHelper.getCurrentSession().load(ArrayDesign.class, design.getId());
-        final Hybridization hyb = project.getExperiment().getHybridizations().iterator().next();
-        final List<AbstractDesignElement> l =
-            hyb.getDerivedDataCollection().iterator().next().getDataSet().getDesignElementList()
-            .getDesignElements();
-        assertEquals(3, l.size());
-        for (final AbstractDesignElement de : l) {
-            final PhysicalProbe p = (PhysicalProbe) de;
-            assertTrue(design.getDesignDetails().getProbes().contains(p));
-        }
-        final List<HybridizationData> hdl =
-            hyb.getDerivedDataCollection().iterator().next().getDataSet().getHybridizationDataList();
-        assertEquals(1, hdl.size());
-        assertEquals(6, hdl.get(0).getColumns().size());
-        final List<String> stringColumns =
-            new ArrayList<String>(Arrays.asList(IlluminaGenotypingProcessedMatrixQuantitationType.ALLELE.getName()));
-        final List<String> floatColumns =
-            new ArrayList<String>(Arrays.asList(
-                    IlluminaGenotypingProcessedMatrixQuantitationType.B_ALLELE_FREQ.getName(),
-                    IlluminaGenotypingProcessedMatrixQuantitationType.GC_SCORE.getName(),
-                    IlluminaGenotypingProcessedMatrixQuantitationType.LOG_R_RATIO.getName(),
-                    IlluminaGenotypingProcessedMatrixQuantitationType.R.getName(),
-                    IlluminaGenotypingProcessedMatrixQuantitationType.THETA.getName()));
-        for (final AbstractDataColumn c : hdl.get(0).getColumns()) {
-            pdp.loadFromStorage(c);
-            final String name = c.getQuantitationType().getName();
-            if (stringColumns.contains(name)) {
-                assertEquals(3, ((StringColumn) c).getValues().length);
-                assertTrue("missing " + c.getQuantitationType(),
-                        stringColumns.remove(c.getQuantitationType().getName()));
-            } else if (floatColumns.contains(name)) {
-                assertEquals(3, ((FloatColumn) c).getValues().length);
-                assertTrue("missing " + c.getQuantitationType(), floatColumns.remove(c.getQuantitationType().getName()));
-            } else {
-                fail("unexpected column: " + name);
-            }
-        }
-        assertTrue("not all columns present", stringColumns.isEmpty() && floatColumns.isEmpty());
         tx.commit();
     }
 }
