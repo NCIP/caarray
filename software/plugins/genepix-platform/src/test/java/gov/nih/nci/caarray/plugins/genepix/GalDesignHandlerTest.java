adcc92dc83ba7cb62a58c1b9de79c5d6af292d3b
Krishna Kanchinadam <kkanchinadam@5amsolutions.com>
2012-02-05 06:36:18 +0000

    array-1692-compilation problems resolved. most of the junits fail.

--- software/plugins/genepix-platform/src/test/java/gov/nih/nci/caarray/plugins/genepix/GalDesignHandlerTest.java
+++ software/plugins/genepix-platform/src/test/java/gov/nih/nci/caarray/plugins/genepix/GalDesignHandlerTest.java
@@ -87,7 +87,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.junit.matchers.StringContains.containsString;
+import static org.junit.internal.matchers.StringContains.containsString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import gov.nih.nci.caarray.dao.ArrayDao;
