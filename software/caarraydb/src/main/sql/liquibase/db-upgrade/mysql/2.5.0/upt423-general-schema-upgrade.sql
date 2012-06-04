fcacbe16f071f4a1fd9fe6709f8000b7e3a2a83c
Paul Shrabstein <shrabsteinp@mail.nih.gov>
2012-06-04 19:26:53 +0000

    ARRAY-2528: Upgrade caArray schema to UPT 4.2.3

---
+++ software/caarraydb/src/main/sql/liquibase/db-upgrade/mysql/2.5.0/upt423-general-schema-upgrade.sql
@@ -0,0 +1,28 @@
+insert csm_protection_group (protection_group_id, protection_group_name, protection_group_description, application_id, large_element_count_flag, update_date)
+values
+(1, 'UPT_UI_USERS_LINK', 'Protection Group representing User link in UPT. Do not change the name.', 1, 0, sysdate());
+
+insert csm_protection_group (protection_group_id, protection_group_name, protection_group_description, application_id, large_element_count_flag, update_date)
+values
+(2, 'UPT_UI_PROTECTION_ELEMENTS_LINK', 'Protection Group representing Protection Element link in UPT. Do not change the name.', 1, 0, sysdate());
+
+insert csm_protection_group (protection_group_id, protection_group_name, protection_group_description, application_id, large_element_count_flag, update_date)
+values
+(3, 'UPT_UI_PRIVILEGES_LINK', 'Protection Group representing Privilege link in UPT. Do not change the name.', 1, 0, sysdate());
+
+insert csm_protection_group (protection_group_id, protection_group_name, protection_group_description, application_id, large_element_count_flag, update_date)
+values
+(4, 'UPT_UI_GROUPS_LINK', 'Protection Group representing Group link in UPT. Do not change the name.', 1, 0, sysdate());
+
+insert csm_protection_group (protection_group_id, protection_group_name, protection_group_description, application_id, large_element_count_flag, update_date)
+values
+(5, 'UPT_UI_PROTECTION_GROUPS_LINK', 'Protection Group representing Protection Group link in UPT. Do not change the name.', 1, 0, sysdate());
+
+insert csm_protection_group (protection_group_id, protection_group_name, protection_group_description, application_id, large_element_count_flag, update_date)
+values
+(6, 'UPT_UI_ROLE_LINK', 'Protection Group representing Role link in UPT. Do not change the name.', 1, 0, sysdate());
+
+insert csm_protection_group (protection_group_id, protection_group_name, protection_group_description, application_id, large_element_count_flag, update_date)
+values
+(7, 'UPT_UI_INSTANCE_LEVEL_LINK', 'Protection Group representing Instance Level link in UPT. Do not change the name.', 1, 0, sysdate());
+
