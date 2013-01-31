3fc389aec41202f819d5eb98ffa04afe0db5b4dc
Winston Cheng <wcheng@5amsolutions.com>
2013-01-31 13:58:27 -0500

    Updated license

--- caArray_v2.2.0GA_mod_caRPLA/CAARRAY_SOFTWARE_CHANGES/rplatab.jar/src/main/java/gov/nih/nci/carpla/rplatab/sradf/SradfHeaderReader.java
+++ caArray_v2.2.0GA_mod_caRPLA/CAARRAY_SOFTWARE_CHANGES/rplatab.jar/src/main/java/gov/nih/nci/carpla/rplatab/sradf/SradfHeaderReader.java
@@ -1,3 +1,9 @@
+//======================================================================================
+// Copyright 5AM Solutions Inc, Yale University, Lawrence Berkeley National Laboratory
+//
+// Distributed under the OSI-approved BSD 3-Clause License.
+// See https://github.com/NCIP/caarray/blob/caRPLA/LICENSE for details.
+//======================================================================================
 package gov.nih.nci.carpla.rplatab.sradf;
 
 import gov.nih.nci.caarray.validation.ValidationMessage.Type;
